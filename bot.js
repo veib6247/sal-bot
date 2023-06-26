1854718d3fd3bef5c700b02daba453ecc220b7a5
Bryan Olandres <ozoneblacklight@outlook.com>
2023-06-26 17:45:26 +0800

    added timezone to cron job for hosting

--- bot.js
+++ bot.js
@@ -6,39 +6,56 @@ import { botClocker } from './utils/puppet.js'
 dotenv.config()
 
 // build clock in schedule
-const clockInHour = 10
 const clockInMins = 0
+const clockInHour = 10
 
 const workDays = 'Monday,Tuesday,Wednesday,Thursday,Friday' // remove weekends on production
 const clockInTime = `${clockInMins} ${clockInHour} * * ${workDays}`
 
 // build clock out schedule
-const clockOutHour = clockInHour + 9 // (clockInHour + 9) to add the whole 8hr shift and 1hr break
 const clockOutMins = 0
+const clockOutHour = clockInHour + 9 // (clockInHour + 9) to add the whole 8hr shift and 1hr break
 
 const clockOutTime = `${clockOutMins} ${clockOutHour} * * ${workDays}`
 
 /**
- * jobs
+ * CLOCK IN
  */
 console.log(`CLOCK_IN will trigger every "${workDays}" @ ${clockInHour}:00 MIL`)
-cron.schedule(clockInTime, async () => {
-  try {
-    await botClocker('clock_in') // ETA < 10 seconds
-  } catch (error) {
-    console.log(error)
-    process.exit(1)
+cron.schedule(
+  clockInTime,
+  async () => {
+    try {
+      await botClocker('clock_in') // ETA < 10 seconds
+    } catch (error) {
+      console.log(error)
+      process.exit(1)
+    }
+  },
+  {
+    scheduled: true,
+    timezone: 'Asia/Taipei',
   }
-})
+)
 
+/**
+ * CLOCK OUT
+ */
 console.log(
   `CLOCK_OUT will trigger every "${workDays}" @ ${clockOutHour}:00 MIL`
 )
-cron.schedule(clockOutTime, async () => {
-  try {
-    await botClocker('clock_out') // ETA < 10 seconds
-  } catch (error) {
-    console.log(error)
-    process.exit(1)
+cron.schedule(
+  clockOutTime,
+  async () => {
+    try {
+      await botClocker('clock_out') // ETA < 10 seconds
+    } catch (error) {
+      console.log(error)
+      process.exit(1)
+    }
+  },
+  {
+    scheduled: true,
+    timezone: 'Asia/Taipei',
   }
-})
+)
